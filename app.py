746c5dcaddbe1aeabc6d2aabc03756e1acbb02fb
Sailajayadav <veerlasailajayadav@gmail.com>
2025-03-15 16:14:53 +0530

    updated frontend

--- app.py
+++ app.py
@@ -92,27 +92,56 @@ try:
             gridoptions2 = gd2.build()
             AgGrid(df_replies.head(10), gridOptions=gridoptions2, theme='streamlit')
             
-            # Sentiment Analysis
-            st.subheader("😊 Sentiment Analysis")
+            # Sentiments of the Commentors
+            st.subheader("Reviews") 
             sentiments = df[df['Language'] == 'English']
-            data_sentiments = sentiments['TextBlob_Sentiment_Type'].value_counts().rename_axis('Sentiment').reset_index(name='counts')
-            data_sentiments['Review_percent'] = (100. * data_sentiments['counts'] / data_sentiments['counts'].sum()).round(1)
+            data_sentiments = sentiments['TextBlob_Sentiment_Type'].value_counts(
+            ).rename_axis('Sentiment').reset_index(name='counts')
+
+            data_sentiments['Review_percent'] = (
+                100. * data_sentiments['counts'] / data_sentiments['counts'].sum()).round(1)
+
             result = data_sentiments.to_json(orient="split")
             parsed = json.loads(result)
-            options = {
-                "tooltip": {"trigger": "item", "formatter": '{d}%'},
-                "series": [{
-                    "name": "Sentiment",
-                    "type": "pie",
-                    "radius": ["40%", "70%"],
-                    "data": [
-                        {"value": parsed['data'][1][2], "name": parsed['data'][1][0]},
-                        {"value": parsed['data'][0][2], "name": parsed['data'][0][0]},
-                        {"value": parsed['data'][2][2], "name": parsed['data'][2][0]}
-                    ],
-                }],
-            }
-            st_echarts(options=options, height="400px")
 
-except Exception as e:
-    st.error(f"❌ Invalid URL! Please enter a valid YouTube link.", icon="🚨")
+            options = {
+                "tooltip": {"trigger": "item",
+                            "formatter": '{d}%'},
+                "legend": {"top": "5%", "left": "center"},
+                "series": [
+                    {
+                        "name": "Sentiment",
+                        "type": "pie",
+                        "radius": ["40%", "70%"],
+                        "avoidLabelOverlap": False,
+                        "itemStyle": {
+                            "borderRadius": 10,
+                            "borderColor": "#fff",
+                            "borderWidth": 2,
+                        },
+                        "label": {"show": False, "position": "center"},
+                        "emphasis": {
+                            "label": {"show": True, "fontSize": "30", "fontWeight": "bold"}
+                        },
+                        "labelLine": {"show": False},
+                        "data": [
+                            # NEUTRAL
+                            {"value": parsed['data'][1][2],
+                             "name": parsed['data'][1][0]},
+                            # POSITIVE
+                            {"value": parsed['data'][0][2],
+                             "name": parsed['data'][0][0]},
+                            # NEGATIVE
+                            {"value": parsed['data'][2][2],
+                             "name": parsed['data'][2][0]}
+                        ],
+                    }
+                ],
+            }
+            st_echarts(
+                options=options, height="500px",
+            )
+
+except:
+    st.error(
+        ' The URL Should be of the form: https://www.youtube.com/watch?v=videoID', icon="🚨")
